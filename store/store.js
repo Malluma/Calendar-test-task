1779eff8938fedf690f52d0dee071e6c86509e78
Malluma <sudzumia@gmail.com>
2022-08-16 16:51:42 +0700

    Updates

--- store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import calendarReducer from './reducers/CalendarSlice'
-
-export const store = configureStore({
-    reducer: {
-      calendar: calendarReducer
-    }
-  })
